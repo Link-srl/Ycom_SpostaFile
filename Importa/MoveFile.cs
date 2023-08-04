3ac8e5a9bdba9875b7069f7dfdd1833ffe6668e8
VincenzoGLink <v.giacalone@linkmes.it>
2023-08-04 10:49:05 +0200

    Aggiunto il foreach con il giusto ciclo

--- Importa/MoveFile.cs
+++ Importa/MoveFile.cs
@@ -20,6 +20,7 @@ namespace ScambioDati
         {
             string sourceFolder = @"C:\Share\WENZEL\TO_Phase\"; // percorso del file di origine
             string destFolder = @"C:\Share\WENZEL\FROM_Phase\"; // percorso del file di destinazione
+
             Zero5.Util.Log.WriteLog("From folder: " + sourceFolder);
             Zero5.Util.Log.WriteLog("Destination folder : " + destFolder);
             string[] files = Directory.GetFiles(sourceFolder); // elenco dei file nella cartella di origine
@@ -34,8 +35,61 @@ namespace ScambioDati
             {
                 string fileName = Path.GetFileName(file); // nome del file
                 Zero5.Util.Log.WriteLog("Nome file che sta venendo elaborato: " + fileName);
-                string destinationFile = Path.Combine(destFolder, fileName); // percorso completo del file di destinazione
-                File.Move(file, destinationFile); // sposta il file nella cartella di destinazione
+                string[] parts = fileName.Split(' ');
+                string code = parts[0];
+                string serial = parts[4];
+                string pattern = @"\d+"; // match one or more digits
+                string digits;
+                string folderCode = destFolder + code;
+                string folderSerial;
+
+                #region cartella con codice
+
+                Zero5.Util.Log.WriteLog("Checking if folder exists: " + folderCode);
+                if (Directory.Exists(folderCode))
+                {
+                    Zero5.Util.Log.WriteLog("Folder already exists.");
+                    // add a suffix to the folder name to create a new folder with a different name                   
+                }
+                else
+                {
+                    Zero5.Util.Log.WriteLog("Folder does not exist.");
+                    Zero5.Util.Log.WriteLog("Creating folder: " + folderCode);
+                    Directory.CreateDirectory(folderCode);
+                }
+
+                #endregion
+
+                #region cartella con seriale
+
+                Match match = Regex.Match(serial, pattern);
+                if (match.Success)
+                {
+                    digits = match.Value;
+                    folderSerial = folderCode + "\\" + digits;
+                    Zero5.Util.Log.WriteLog(digits); // shows in log what we are taking from the serial number
+                    Zero5.Util.Log.WriteLog("Checking if folder exists: " + folderSerial);
+                    if (Directory.Exists(folderSerial))
+                    {
+                        Zero5.Util.Log.WriteLog("Folder already exists.");
+                        // add a suffix to the folder name to create a new folder with a different name                       
+                    }
+                    else
+                    {
+                        Zero5.Util.Log.WriteLog("Folder does not exist.");
+                        Zero5.Util.Log.WriteLog("Creating folder: " + folderSerial);
+                        Directory.CreateDirectory(folderSerial);
+                    }
+                    string destinationFile = Path.Combine(folderSerial, fileName);
+
+                    File.Move(file, destinationFile);
+                }
+                else
+                {
+                    Zero5.Util.Log.WriteLog("No digits found.");
+                }
+
+                #endregion
             }
 
             Zero5.Util.Log.WriteLog("Spostamento completato.");
